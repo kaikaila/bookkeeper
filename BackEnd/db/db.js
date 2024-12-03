36a40e0c7b486af5d7b5b3899b6164415ff84c90
kaikaila <lyk2772@126.com>
2024-12-02 20:48:44 -0800

    controller-income is done

--- BackEnd/db/db.js
+++ BackEnd/db/db.js
@@ -1,3 +1,5 @@
+// 出现 db connection errors时，可能是mongoDB没有同步上本机现在的IP地址，去官网添加即可
+
 const { config } = require("dotenv");
 const mongoose = require("mongoose");
 
--- BackEnd/routes/transactions.js
+++ BackEnd/routes/transactions.js
@@ -1,9 +1,20 @@
 const { Router } = require("express");
+const {
+  addIncome,
+  getIncomes,
+  deleteIncomes,
+} = require("../controllers/income");
 
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("hello world from transaction router");
-});
+// router.get("/", (req, res) => {
+//   res.send("hello world from transaction router");
+// });
+// addIncome comes from controller
+// user reach endpoint '/add-in' will trigger a post
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIncomes)
+  .delete("/delete-income/:id", deleteIncomes);
 
 module.exports = router;
