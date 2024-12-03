282b384a662ac5be0144bf3f7b26bac6c4d195b7
kaikaila <lyk2772@126.com>
2024-12-03 00:00:12 -0800

    before deleting all wrongly installed packages on the Bookkeeping folder

---
+++ bookkeeperfront/src/App.js
@@ -0,0 +1,11 @@
+import { GlobalStyle } from "./styles/GlobalStyle";
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
