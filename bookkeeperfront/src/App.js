8705f702a5d723bbd3fe70b0b67d606fb034a780
kaikaila <lyk2772@126.com>
2024-12-03 19:53:07 -0800

    main layout and InnerLayout done

--- bookkeeperfront/src/App.js
+++ bookkeeperfront/src/App.js
@@ -1,10 +1,20 @@
 import { GlobalStyle } from "./styles/GlobalStyle";
+import styled from "styled-components";
+import bg from "./img/background.jpg";
+
 function App() {
+  const AppStyled = styled.div`
+  height = 100vh;
+  // 下文这样定义的bg 图片的方式不能把图片置底
+  // background-image:url(bg)
+  background-mage:url(${(props) => props.bg});
+  position:relative;
+   `;
+
   return (
-    <div className="App">
-      <GlobalStyle />
-      <header className="App-header"></header>
-    </div>
+    <AppStyled bg={bg} className="App">
+      <main></main>
+    </AppStyled>
   );
 }
 
