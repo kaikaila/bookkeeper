8cea067441caaba05648d7692a10f893ccbbec52
kaikaila <lyk2772@126.com>
2024-12-09 10:25:21 -0800

    Incomes, IncomeItem, globalContext.js

--- bookkeeperfront/src/components/Incomes/Incomes.js
+++ bookkeeperfront/src/components/Incomes/Incomes.js
@@ -5,6 +5,11 @@ import { useGlobalContext } from "../../context/globalContext";
 import Form from "../Form/Form";
 
 function Incomes() {
+  const { addIncome, incomes, getIncomes, deleteIncome, totalIncome } =
+    useGlobalContext();
+  useEffect(() => {
+    getIncomes();
+  }, []);
   return (
     <IncomesStyled>
       <InnerLayout>
@@ -13,7 +18,26 @@ function Incomes() {
           <div className="form-container">
             <Form />
           </div>
-          <div className="incomes"></div>
+          <div className="incomes">
+            {incomes.map((income) => {
+              const { _id, title, amount, date, category, description, type } =
+                income;
+              return (
+                <IncomeItem
+                  key={_id}
+                  id={_id}
+                  title={title}
+                  description={description}
+                  amount={amount}
+                  date={date}
+                  type={type}
+                  category={category}
+                  indicatorColor="var(--color-green)"
+                  deleteItem={deleteIncome}
+                />
+              );
+            })}
+          </div>
         </div>
       </InnerLayout>
     </IncomesStyled>
