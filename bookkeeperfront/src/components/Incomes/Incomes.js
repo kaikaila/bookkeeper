10c4028549a5dec78fbb3dc560b47986643202ed
kaikaila <lyk2772@126.com>
2024-12-09 11:08:37 -0800

    corrected income.js styled

--- bookkeeperfront/src/components/Incomes/Incomes.js
+++ bookkeeperfront/src/components/Incomes/Incomes.js
@@ -15,6 +15,9 @@ function Incomes() {
     <IncomesStyled>
       <InnerLayout>
         <h1>Incomes</h1>
+        <h2 className="total-income">
+          Total Income: <span>${totalIncome()}</span>
+        </h2>
         <div className="income-content">
           <div className="form-container">
             <Form />
@@ -44,5 +47,33 @@ function Incomes() {
     </IncomesStyled>
   );
 }
-const IncomesStyled = styled.div``;
+const IncomesStyled = styled.div`
+  display: flex;
+  overflow: auto;
+  .total-income {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #fcf6f9;
+    border: 2px solid #ffffff;
+    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+    border-radius: 20px;
+    padding: 1rem;
+    margin: 1rem 0;
+    font-size: 2rem;
+    gap: 0.5rem;
+    span {
+      font-size: 2.5rem;
+      font-weight: 800;
+      color: var(--color-green);
+    }
+  }
+  .income-content {
+    display: flex;
+    gap: 2rem;
+    .incomes {
+      flex: 1;
+    }
+  }
+`;
 export default Incomes;
